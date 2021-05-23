eb2a0b5fe62a34c6a24afe00b0aa274d5504ba03
Robert Curry <robert.curry@lexer.io>
2021-05-23 10:18:52 +1000

    Back light added, Hyper added, Alt added to arrow layer

--- keyboards/planck/keymaps/RobertCurry0216/keymap.c
+++ keyboards/planck/keymaps/RobertCurry0216/keymap.c
@@ -47,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   M  |   C  |   V  |   K  |   L  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  | xxx  | GUI  |Lower |    Space    |Raise |Bright| xxx  | xxx  | xxx  |
+ * | Ctrl | Alt  |Hyper | GUI  |Lower |    Space    |Raise |BLtog |BLdwn | BLup |BLcyc |
  * `-----------------------------------------------------------------------------------'
  */
 [_WORKMAN] = LAYOUT_planck_grid(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LT_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT_QUOT,
-    SFT_CAPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL, KC_LALT, _______, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   _______, _______, _______, _______
+    SFT_CAPS, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_LCTL, KC_LALT, KC_HYPR, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   BL_TOGG, BL_DEC, BL_INC, BL_STEP
 ),
 
 /* Raise
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_HOME, KC_UP,   KC_END,  _______, _______,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, KC_LALT, KC_LALT, _______, _______, _______, _______, _______
 ),
 
 /* Symbols
